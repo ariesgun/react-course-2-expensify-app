59759ed6e440f3470c4c56ce0221c44314177cfa
Aries Thio <aries.thio@hotmail.com>
2018-04-21 11:05:10 +0200

    Setup test database env

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -2,18 +2,39 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import ExpenseForm from './ExpenseForm.js';
-import { addExpense } from '../actions/expenses.js';
+import { startAddExpense } from '../actions/expenses.js';
 
-const AddExpensePage = (props) => (
-  <div>
-    <h1>Add Expense</h1>
-    <ExpenseForm 
-      onSubmit={(expense) => {
-        props.dispatch(addExpense(expense))
-        props.history.push('/');
-      }}
-    />
-  </div>
-);
+// const AddExpensePage = (props) => (
+//   <div>
+//     <h1>Add Expense</h1>
+//     <ExpenseForm 
+//       onSubmit={(expense) => {
+//         props.dispatch(addExpense(expense))
+//         props.history.push('/');
+//       }}
+//     />
+//   </div>
+// );
 
-export default connect()(AddExpensePage);
+export class AddExpensePage extends React.Component {
+  onSubmit = (expense) => {
+    this.props.startAddExpense(expense);
+    this.props.history.push('/');
+  };
+  render() {
+    return (
+      <div>
+        <h1>Add Expense</h1>
+        <ExpenseForm
+          onSubmit={this.onSubmit}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  startAddExpense: (expense) => dispatch(startAddExpense(expense))
+});
+
+export default connect(undefined, mapDispatchToProps)(AddExpensePage);

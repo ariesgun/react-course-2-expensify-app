77be99e0da6bc4bc5efe1d218eb75b99f24cc040
Aries Thio <aries.thio@hotmail.com>
2018-04-22 00:41:46 +0200

    Styling Expensify App

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -2,9 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ExpenseList from './ExpenseList.js';
 import ExpenseListFilters from './ExpenseListFilters.js'
+import ExpensesSummary from './ExpensesSummary';
 
 const ExpenseDashboardPage = () => (
   <div>
+    <ExpensesSummary />
     <ExpenseListFilters />
     <ExpenseList />
   </div>
--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 
 const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }) => (
-  <div>
-    <Link to={`/edit/${id}`}>
-      <h3>{description}</h3>
+    <Link className="list-item" to={`/edit/${id}`}>
+      <div>
+        <h3 className="list-item__title">{description}</h3>
+        <span className="list-item__sub-title">{moment(createdAt).format('MMM Do, YYYY')}</span>
+      </div>
+      <h3 className="list-item__data">
+        {numeral(amount / 100).format('$0,0.00')} 
+      </h3>
     </Link>
-    <p>${amount} - {moment(createdAt).format('MMM Do, YYYY')}</p>
-  </div>
 );
 
 const mapStateToProps = (state) => {

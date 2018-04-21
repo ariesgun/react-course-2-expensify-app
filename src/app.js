9c30b73ab7b20e6678160db388118f0f3f215a52
Aries Thio <aries.thio@hotmail.com>
2018-04-21 11:31:41 +0200

    Get firebase expenses on start

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import AppRouter from './routers/AppRouter.js';
 import configureStore from './store/configureStore.js';
-import {addExpense} from './actions/expenses.js';
+import {addExpense, startSetExpenses} from './actions/expenses.js';
 import {setTextFilter} from './actions/filters.js';
 import getVisibleExpenses from './selectors/expenses.js';
 import 'normalize.css/normalize.css';
@@ -13,19 +13,19 @@ import './firebase/firebase.js';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 2000, createdAt: 2000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 200, createdAt: 500 }));
-store.dispatch(setTextFilter('water'));
+// store.dispatch(addExpense({ description: 'Water Bill', amount: 100, createdAt: 1000 }));
+// store.dispatch(addExpense({ description: 'Gas Bill', amount: 2000, createdAt: 2000 }));
+// store.dispatch(addExpense({ description: 'Rent', amount: 200, createdAt: 500 }));
+// store.dispatch(setTextFilter('water'));
 
-setTimeout(() => {
-  store.dispatch(setTextFilter('bill'));
-}, 3000);
+// setTimeout(() => {
+//   store.dispatch(setTextFilter('bill'));
+// }, 3000);
 
-const state = store.getState();
-console.log(state);
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filter);
-console.log(visibleExpenses);
+// const state = store.getState();
+// console.log(state);
+// const visibleExpenses = getVisibleExpenses(state.expenses, state.filter);
+// console.log(visibleExpenses);
 
 const jsx = (
   <Provider store={store}>
@@ -33,4 +33,9 @@ const jsx = (
   </Provider>
 );
 
-ReactDOM.render(jsx , document.getElementById('app'));
+ReactDOM.render(<p>Loading...</p> , document.getElementById('app'));
+
+store.dispatch(startSetExpenses()).then(() => {
+  ReactDOM.render(jsx , document.getElementById('app'));
+});
+
